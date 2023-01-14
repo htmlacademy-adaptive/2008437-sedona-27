8e76f57fa6019dfc8b40f0beb38f4b976eea4f28
Vyacheslav Andreev <97975418+Vyachowski@users.noreply.github.com>
2023-01-14 20:00:20 +0200

    Добавляет исправления в шапку

--- source/js/app.js
+++
@@ -1,7 +0,0 @@
-const mobileMenuButton = document.querySelector('.main-nav__toggle');
-const mobileMenu = document.querySelector('.site-list');
-
-const menuToggle = () => {
-mobileMenuButton.classList.toggle('main-nav__toggle--closed');
-mobileMenu.classList.toggle('site-list--opened');
-}
