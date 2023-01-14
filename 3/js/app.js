15b052b5f7bb0ccb91920db4024a524d28daf031
Keks <keks@htmlacademy.ru>
2023-01-14 13:54:01 +0300

    :heavy_check_mark: Сборка #3

--- 3/js/app.js
+++ 3/js/app.js
@@ -1,5 +1,5 @@
-let mobileMenuButton = document.querySelector('.main-nav__toggle');
-let mobileMenu = document.querySelector('.site-list');
+const mobileMenuButton = document.querySelector('.main-nav__toggle');
+const mobileMenu = document.querySelector('.site-list');
 
 const menuToggle = () => {
 mobileMenuButton.classList.toggle('main-nav__toggle--closed');
